d6ca415cf0bf9b86af2dfab4f607f95632773512
jthetzel <jthetzel@gmail.com>
2018-08-22 16:25:32 -0230

    Authenticate all services

--- src/services/users/users.hooks.js
+++ src/services/users/users.hooks.js
@@ -6,17 +6,33 @@ const {
 
 module.exports = {
   before: {
-    all: [],
-    find: [ authenticate('jwt') ],
-    get: [ authenticate('jwt') ],
-    create: [ hashPassword() ],
-    update: [ hashPassword(),  authenticate('jwt') ],
-    patch: [ hashPassword(),  authenticate('jwt') ],
-    remove: [ authenticate('jwt') ]
+    all: [
+    ],
+    find: [
+      authenticate('jwt')
+    ],
+    get: [
+      authenticate('jwt')
+    ],
+    create: [
+      hashPassword(),
+      authenticate('jwt')
+    ],
+    update: [
+      hashPassword(),
+      authenticate('jwt')
+    ],
+    patch: [
+      hashPassword(),
+      authenticate('jwt')
+    ],
+    remove: [
+      authenticate('jwt')
+    ]
   },
 
   after: {
-    all: [ 
+    all: [
       // Make sure the password field is never sent to the client
       // Always must be the last hook
       protect('password')
